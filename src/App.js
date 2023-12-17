606e014aac181fdaecaeb99f62c8f8194ae16f6e
Mahamadou <mmohmo@yahoo.com>
2023-12-16 22:13:45 -0500

    tailwind css loaded

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Seyba</h2>
+      <h2 className="bg-slate-950 text-slate-500 text-center">Seyba</h2>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
-    //"./node_modules/tw-elements-react/dist/js/**/*.js"
+    "./src/**/*.{html,js}", 
+    "./node_modules/tw-elements-react/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [
-    // require("tw-elements-react/dist/plugin.cjs" ),
-    // require('tailwindcss-animated')
+    require("tw-elements-react/dist/plugin.cjs" ),
+    require('tailwindcss-animated')
   ]
 }
