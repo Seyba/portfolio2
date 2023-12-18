7ba5ea33aeaa94926801fd2f1152bc8dcdc8572d
Mahamadou <mmohmo@yahoo.com>
2023-12-17 21:56:43 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Navbar2 } from './component/Navbar2';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="bg-slate-950 text-slate-500 text-center">Seyba</h2>
+    <div className="bg-slate-950">
+      <Navbar2/>
+      
     </div>
   );
 }
