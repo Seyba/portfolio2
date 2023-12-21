c2b02561b23efab0c893fd07f40518743ff72a0a
Mahamadou <mmohmo@yahoo.com>
2023-12-20 22:55:20 -0500

    footer info section styled

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,36 +4,43 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <footer className="p-8">
-        <div className="md:grid grid-cols-2 gap-32">
-            <div className="">
-                <h4>Seyba</h4>
-                <p>I'am an award UI/UX designer, web developer and product manager based in New York, USA.</p>
+        <div className="footer-wrapper">
+            <div className="md:grid grid-cols-2 gap-32">
                 <div className="">
-                    <ul className="flex">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                    </ul>
+                    <div className="py-4">
+                        <h4 className="font-semibold text-3xl">SEYBA</h4>
+                    </div>
+                    <div>
+                        <p className="text-lg leading-8 py-4">I'am an award UI/UX designer, web developer and product manager based in New York, USA.</p>
+                    </div>
+                    <div className="py-4">
+                        <ul className="flex">
+                            <li className="pr-4">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="pr-4">
+                                <Link to="/">About</Link>
+                            </li>
+                            <li className="pr-4">
+                                <Link to="/">Blog</Link>
+                            </li>
+                            <li className="pr-4 ">
+                                <Link to="/">Home</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>
+                        &copy; {new Date().getFullYear()}; Powered by <Link className="text-white" to="https://github.com/Seyba">SEYBA</Link>
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p>
-                    &copy; {new Date().getFullYear()}; Powered by <Link className="text-white" to="https://github.com/Seyba">SEYBA</Link>
-                    </p>
-                </div>
-            </div>
-            <div className="">
-                <div>
-                    <p>mmohmo@yahoo.com</p>
-                    <p>+1 (646) 479 9306</p>
+                <div className="">
+                    <div>
+                        <p>mmohmo@yahoo.com</p>
+                        <p>+1 (646) 479 9306</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
