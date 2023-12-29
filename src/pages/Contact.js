aca5d26657926e4f2166b0d59b13766b622b55e0
= <mmohmo@yahoo.com>
2023-12-28 22:37:59 -0500

    contact page heading added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Meta from '../component/Meta'
+import { BreadCrumb } from '../component/BreadCrumb'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div className="flex justify-around mb-4">
+        <div>
+          <h2 className="text-5xl font-semibold text-center py-2">Contact Me</h2>
+          <p className="text-gray-300 text-center text-2xl py-2 leading-8 tracking-wider">Let's work together.</p>
+        </div>
+      </div>
+      <Meta title={"Contact"}/>
+      <BreadCrumb title="Contact"/>
+    </>
   )
 }
