6d5db09b6da3f66c4bd385acf68a2d6f13123d1a
= <mmohmo@yahoo.com>
2023-12-29 23:40:25 -0500

    background image loaded

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,7 @@ export const Projects = () => {
   return (
     <>
         <div className="hero">    
-            <div className="flex justify-around mb-4">
+            <div className="hero-text flex justify-around mb-4">
                 <div>
                     <h2 className="text-5xl font-semibold text-center py-2">Projects</h2>
                     <p className="text-gray-300 text-center font-light text-3xl py-2 leading-8 tracking-wider">I design awesome things.</p>
@@ -14,6 +14,7 @@ export const Projects = () => {
             </div>
             <Meta title={"Projects"}/>
             <BreadCrumb title="Projects"/>
+            <img src="/images/bg-pic.png" alt="hero" className="hero-bg-img"/>
         </div>
     </>
   )
