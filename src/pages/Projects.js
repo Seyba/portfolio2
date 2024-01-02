c124441ccf8cf6937167797c5b1f64e80fe81035
= <mmohmo@yahoo.com>
2024-01-01 23:47:30 -0500

    banner background image added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,16 +5,19 @@ import { BreadCrumb } from '../component/BreadCrumb'
 export const Projects = () => {
   return (
     <>
-        <div className="">    
-            <div className="hero-text-wrapper ">
-                <div>
-                    <h2 className="text-5xl font-semibold text-center py-2">Projects</h2>
-                    <p className="text-gray-300 text-center font-light text-3xl py-2 leading-8 tracking-wider">I design awesome things.</p>
+        <div>  
+            
+            <div className="banner-wrapper">
+                <div className="banner">
+                    <div>
+                        <h2 className="text-5xl font-semibold text-center py-2">Projects</h2>
+                        <p className="text-gray-300 text-center font-light text-3xl py-2 leading-8 tracking-wider">I design awesome things.</p>
+                    </div>
+                    <Meta title={"Projects"}/>
+                    <BreadCrumb title="Projects"/>
                 </div>
             </div>
-            <Meta title={"Projects"}/>
-            <BreadCrumb title="Projects"/>
-            <img src="/images/bg-pic.png" alt="hero" className="hero-bg-img"/>
+                
         </div>
     </>
   )
