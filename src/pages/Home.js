ffcdbe9f9dfcff886b01ce1e2fc9e2ccfb05512e
= <mmohmo@yahoo.com>
2024-01-02 21:57:25 -0500

    home page initialized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className="home__hero flex justify-around">
+        <div>
+          <h4 className="text-xl font-semibold text-center py-4">Digital experience, made better</h4>
+          <div>
+            <h1>We are a</h1>
+            <div className="word-wrap">
+              <h1>creative</h1>
+              <h1>innovative</h1>
+              <h1>powerful</h1>
+              <h1>talented</h1>
+            </div>
+            <h1>digital studio.</h1>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
