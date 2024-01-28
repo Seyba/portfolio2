8228f760f5184547404960d53f59a2eab5c0739a
= <mmohmo@yahoo.com>
2024-01-28 14:54:36 -0500

    block title updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ export const Home = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       const idx = Math.floor(Math.random() * text.length)
-
       setMsgIdx(previous => {
         if(previous === msg.length) {
           return 0;
@@ -285,8 +284,8 @@ export const Home = () => {
         <div className="md:grid grid-cols-2 place-items-center gap-2">
           <div className="max-w-md text-left">
             <h2 className="text-lg font-semibold tracking-wide mb-4">Why work with me</h2>
-            <h3 className="text-3xl font-bold mb-4">Top five reasons to work with me.</h3>
-            <h4 className="text-xl leading-6 tracking-wide font-thin text-slate-200 mb-4">I'm a subhead that goes with a story.</h4>
+            <h3 className="text-5xl font-bold mb-4 tracking-loose">Top five reasons to work with me.</h3>
+            <h4 className="text-2xl leading-9 tracking-wide font-normal text-slate-200 mb-4">I'm a subhead that goes with a story.</h4>
             <div className="flex flex-wrap">
               <div className="flex ">
                 <div className="flex justify-center items-center bg-neutral-800 rounded-full mr-4 py-2 my-3">
