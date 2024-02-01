05ef6196edd5eef83ca0f4b04eb99cb0efecf6b5
= <mmohmo@yahoo.com>
2024-01-31 23:21:15 -0500

    main bg class added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -508,7 +508,7 @@ export const Home = () => {
           
         </div>
       </div>
-      <div className="bg-gray-500">
+      <div className="">
         <ContactForm/>
       </div>
     </div>
