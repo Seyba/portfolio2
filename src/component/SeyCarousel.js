70e48af259dc508e82988366d466602fd049bdc9
= <mmohmo@yahoo.com>
2024-02-03 14:57:31 -0500

    testimonial user images added

--- src/component/SeyCarousel.js
+++ src/component/SeyCarousel.js
@@ -3,51 +3,18 @@ import { Carousel } from 'antd';
 
 export const SeyCarousel = () => {
     const data = [
-        {item: 'one'},
-        {item: 'two'},
-        {item: 'three'}
-        
+       {id: 'a2c', imgSr:"", msg:"", customer:""} 
     ]
-    const [index, setIndex] = useState(data.length > 3 ? data.length - 1 : 2 )
-    const [right, setRight] = useState(1)
-    const [current, setCurrent] = useState(0)
-    const [left, setLeft] = useState(-1)
-
-    const next = () => {
-        setCurrent(current === index ? 0 : current + 1)
-        setRight(right === index ? 0 : right + 1)
-        setLeft(left === index ? 0 : left + 1)
-    }
-    const prev = () => {
-        setCurrent(current === 0 ? index : current - 1)
-        setRight(right === 0 ? index : right - 1)
-        setLeft(left === 0? index : left - 1)
-    }
     return(
-        <div className="flex flex-row ml-auto mr-auto">
-            <button className="bg-stone-500 text-xl p-2" onClick={() => prev()}>
-                {"\u2770"}
-            </button>
-            {
-                [left, current, right].map((idx) => (
-                    <div
-                        key={idx}
-                        className="flex justify-center ml-5 mr-5 w-[15vw] h-[15vh] border-2 border-black"
-                    >
-                        {
-                            data[idx] ? (
-                                <div className="flex flex-col items-center ml-5 mr-5 w-[15vw] h-[15vh]">
-                                    {data[idx].item}
-                                </div>
-                            ):("____")
-                        }
-                    </div>
-                ))
-            }
-            <button className="bg-stone-500 text-xl p-2" onClick={() => next()}>
-                {"\u2771"}
-            </button>
-        </div>
+        <Carousel autoplay>
+            <div className="flex justify-center ">
+                <div className="">
+                    <img src="" alt=""/>
+                    <p></p>
+                    <h4></h4>
+                </div>
+            </div>
+        </Carousel>
     )
 }
 
