f5b3b1eb2cc8f9a2df2655fbad5fd55d757e9f6c
= <mmohmo@yahoo.com>
2024-02-04 14:37:09 -0500

    carousel images loaded

--- src/component/SeyCarousel.js
+++ src/component/SeyCarousel.js
@@ -28,65 +28,36 @@ export const SeyCarousel = () => {
         }
     ]
     const antContent = {
-        height: '200px',
-        padding: '200px 0',
+        height: '400px',
+        padding: '50px 0',
         color: '#fff',
         lineHeight: '16px',
         textAlign: 'center',
-        background: '#3b4149',
+        background: '#373637',
     }
     return(
-        <Carousel className=""> 
+        <Carousel className="mt-6"> 
             <div>
                 <div style={antContent}>
-                    <h3>image</h3>
-                    <h4>dkkkj</h4>
-                    <p>m</p>
-                </div>
-            </div>
-            <div>
-                <div style={antContent}>
-                    
-                    <div className="carousel-img">
-                    </div>
-                    
-                    <h4>oscar</h4>
-                    <p>Mohmo</p>
+                    <div className="carousel-img"/>
+                    <h4>{data[0].msg}</h4>
+                    <p>{data[0].customer}</p>
                 </div>
             </div>
             <div>
                 <div style={antContent}>
-                    <h3>image</h3>
-                    <h4>dkkkj</h4>
-                    <p>m</p>
+                    <div className="carousel-img"/>
+                    <h4>{data[1].msg}</h4>
+                    <p>{data[1].customer}</p>
                 </div>
             </div>
-            {/* <div className="">
-                <div className=""> 
-                    <img src="/images/customer1.jpg" alt="testimonials" className="text-center rounded-full h-16 w-16"/>
+            <div>
+                <div style={antContent}>
+                    <div className="carousel-img"/> 
+                    <h4>{data[2].msg}</h4>
+                    <p>{data[2].customer}</p>
                 </div>
-                <p >Tedkdkt kdd</p>
-                <h4>John doe</h4>
-                <h4>customer</h4>
             </div>
-            <div className="text-slate-100">
-                <div>
-                    <img src="/images/customer1.jpg" alt="testimonials" className="rounded-full h-10 w-10"/>
-                </div>
-                <p>Tedkdkt kdd</p>
-                <h4>Mohmo </h4>
-                <h4>customer</h4>
-            </div>
-            <div className="text-slate-100">
-                <div>
-                    <img src="/images/customer1.jpg" alt="testimonials" className="rounded-full h-10 w-10"/>
-                </div>
-                <p>Tedkdkt kdd</p>
-                <h4>Jane Doe</h4>
-                <h4>customer</h4>
-            </div> */}
-                        
-                
             
         </Carousel>
     )
