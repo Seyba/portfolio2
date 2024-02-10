216135d34caf044ced94975267bc03089353b591
= <mmohmo@yahoo.com>
2024-02-09 22:04:16 -0500

    bio text updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,13 +41,14 @@ export const About = () => {
           </div>
           <div>
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-              <p className="text-base sm:lg">
-                I am a New York City based Full Stack Web Developer with a focus on web design, illustration, a visual development.  I have a diverse range of experience having worked across various fields and industries.
+              <p className="text-xl leading-9 tracking-wide py-12 text-gray-500">
+                I am a New York City based Full Stack Web Developer with a focus on web design, illustration, a visual development.
+                I am passionate about transforming visionary concepts into immersive digital experiences. With expertise in both front and back-end technologies,
+                mission is to craft solutions that exceed expectations. 
                 I have professional experience in customer service, in teaching and hospitality.
                 I am proficient in JavaScript, HTML/CSS, React, Firebase, bootstrap, Sass and Tailwind CSS.
               </p>
             </div>
-            
           </div>
         </div>
       </div>
