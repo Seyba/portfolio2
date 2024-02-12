a9b0ad82a7c2e4d372f056335075e60026e74317
= <mmohmo@yahoo.com>
2024-02-11 21:51:38 -0500

    smooth scrolling added to home contact section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -549,17 +549,18 @@ export const Home = () => {
         </div>
       </div>
       <div className="mt-16 px-16 flex justify-center py-24 bg-gradient-to-r from-zinc-900 via-zinc-700 to-zinc-900">
-        <div className="max-w-2xl text-center">
-          <h3 className="text-base font-semibold mb-4 tracking-widest ">Stay in touch</h3>
-          <h2 className="text-6xl font-bold tracking-wider py-2">Ready to talk</h2>
-          <p className="py-4 text-2xl font-thin leading-relaxed tracking-wider">Feel free to contact me</p>
-          <div className="flex justify-center">
-            <Link className="sBtn" to="/contact">
-              Talk to me
-            </Link>
+        <SmoothScrolling>
+          <div className="max-w-2xl text-center">
+            <h3 className="text-base font-semibold mb-4 tracking-widest ">Stay in touch</h3>
+            <h2 className="text-6xl font-bold tracking-wider py-2">Ready to talk</h2>
+            <p className="py-4 text-2xl font-thin leading-relaxed tracking-wider">Feel free to contact me</p>
+            <div className="flex justify-center">
+              <Link className="sBtn" to="/contact">
+                Talk to me
+              </Link>
+            </div>
           </div>
-          
-        </div>
+        </SmoothScrolling>
       </div>
       <div className="main-bg ">
         <ContactForm/>
