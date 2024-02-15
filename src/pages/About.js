8afbc665a14f3fe74ca7c863e5010e143ffb3df1
= <mmohmo@yahoo.com>
2024-02-15 16:13:19 -0500

    education icons added

--- src/pages/About.js
+++ src/pages/About.js
@@ -128,17 +128,26 @@ export const About = () => {
             <div 
               className="flex justify-between items-center mt-6 mb-2 border-b border-slate-800 py-2"
             >
-              <div>
-                <h4 className="text-lg text-gray-300 font-thin tracking-tight">
-                  <SmoothScrolling>
-                    Per Scholas
-                  </SmoothScrolling>
-                </h4>
-                <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
-                  <SmoothScrolling>
-                    Google Sponsored Software Engineer Certification
-                  </SmoothScrolling>
-                </h4>
+              <div className="flex items-center justify-betwen">
+                <SmoothScrolling>
+                  <div className="flex items-center pl-2 h-12 w-12 bg-slate-700 rounded-full mr-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                    </svg>
+                  </div>
+                </SmoothScrolling>
+                <div>
+                  <h4 className="text-lg text-gray-300 font-thin tracking-tight">
+                    <SmoothScrolling>
+                      Per Scholas
+                    </SmoothScrolling>
+                  </h4>
+                  <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
+                    <SmoothScrolling>
+                      Google Sponsored Software Engineer Certification
+                    </SmoothScrolling>
+                  </h4>
+                </div>
               </div>
               <div>
                 <h3 className="text-lg text-gray-300 font-thin tracking-tight">
@@ -148,39 +157,32 @@ export const About = () => {
                 </h3>
               </div>
             </div>
+            
             <div 
               className="flex justify-between items-center mt-6 mb-2 border-b border-slate-800 py-2"
             >
               <div className="flex items-center justify-betwen">
-                <div className="flex items-center pl-2 h-12 w-12 bg-slate-700 rounded-full mr-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-                  </svg>
+                <div>
+                  <SmoothScrolling>
+                    <div className="flex items-center pl-2 h-12 w-12 bg-slate-700 rounded-full mr-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                      </svg>
+                    </div>
+                  </SmoothScrolling>
                 </div>
                 <div>
-                  <h3>one</h3>
-                  <h3>two</h3>
+                  <h4 className="text-lg text-gray-300 font-thin tracking-tight">
+                    <SmoothScrolling>
+                      Udemy
+                    </SmoothScrolling>
+                  </h4>
+                  <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
+                    <SmoothScrolling>
+                      Javascript | NodeJs | ReactJS Certification
+                    </SmoothScrolling>
+                  </h4>
                 </div>
-              </div>
-              
-              <div>
-                <h3>three</h3>
-              </div>
-            </div>
-            <div 
-              className="flex justify-between items-center mt-6 mb-2 border-b border-slate-800 py-2"
-            >
-              <div>
-                <h4 className="text-lg text-gray-300 font-thin tracking-tight">
-                  <SmoothScrolling>
-                    Udemy
-                  </SmoothScrolling>
-                </h4>
-                <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
-                  <SmoothScrolling>
-                    Javascript | NodeJs | ReactJS Certification
-                  </SmoothScrolling>
-                </h4>
               </div>
               <div>
                 <h3 className="text-lg text-gray-300 font-thin tracking-tight">
@@ -194,17 +196,28 @@ export const About = () => {
             <div 
               className="flex justify-between items-center mt-6 mb-2 border-b border-slate-800 py-2"
             >
-              <div>
-                <h4 className="text-lg text-gray-300 font-thin tracking-tight">
+              <div className="flex justify-between items-center">
+                <div>
                   <SmoothScrolling>
-                    University of Bamako (MALI)
+                    <div className="flex items-center pl-2 h-12 w-12 bg-slate-700 rounded-full mr-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                      </svg>
+                    </div>
                   </SmoothScrolling>
-                </h4>
-                <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
-                  <SmoothScrolling>
-                    Bachelor in Art
-                  </SmoothScrolling>
-                </h4>
+                </div>
+                <div>
+                  <h4 className="text-lg text-gray-300 font-thin tracking-tight">
+                    <SmoothScrolling>
+                      University of Bamako (MALI)
+                    </SmoothScrolling>
+                  </h4>
+                  <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
+                    <SmoothScrolling>
+                      Bachelor in Art
+                    </SmoothScrolling>
+                  </h4>
+                </div>
               </div>
               <div>
                 <h3 className="text-lg text-gray-300 font-thin tracking-tight">
