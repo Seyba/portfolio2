c867c58d28db4bb62018092ad58a07a3cc9e09f0
= <mmohmo@yahoo.com>
2024-02-18 18:50:54 -0500

    project item component added

---
+++ src/component/ProjectItem.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+export const ProjectItem = (props) => {
+    const { id, title, picture, desc, url} = props.projects
+    return(
+        <div>
+            <div className="bg-white rounded-3xl mb-3">
+                <Link to={`${url}`}>
+                    <img 
+                        src={picture} 
+                        alt={id}
+                        className="h-auto max-w-full"
+                    />
+                </Link>
+                <h3 className="text-gray-400 text-lg font-normal text-center">{title}</h3>
+                <h3 className="text-slate-950 text-xl font-semibold text-center">{desc}</h3>
+            </div>
+        </div>
+    )
+}
