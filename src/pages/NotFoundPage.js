6c976f9e6c94bfa9ee8ce053e78117cc8ea3a32a
= <mmohmo@yahoo.com>
2024-02-29 21:48:39 -0500

    Not Found page updated

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 export const NotFoundPage = () => {
     return(
-        <main className="grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid min-h-full place-items-center bg-gray-950 px-6 py-24 sm:py-32 lg:px-8">
             <div className="text-center">
                 <p className="text-base font-bold text-slate-100">404</p>
-                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-300 sm:text-5xl">Page not found</h1>
                 <p className="mt-6 text-base leading-7 text-gray-100">Sorry, we couldn’t find the page you’re looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Link 
