b33f1526c43dec37c4058ed44509e545719c85a3
= <mmohmo@yahoo.com>
2024-02-29 20:34:26 -0500

    single project banner added

--- src/component/ProjectItem.js
+++ src/component/ProjectItem.js
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom"
 import { Banner } from "./Banner"
 import { SmoothScrolling } from "./SmoothScrolling"
+import Meta from "./Meta"
+import { BreadCrumb } from "./BreadCrumb"
+
 export const ProjectItem = (props) => {
     const { id, name, title, address, img} = props.projects
     return(
-        <div className="flex justify-center md:p-4 px-0">
-            <SmoothScrolling>
-
-            
-            <div className="project-bg rounded-sm mb-3 p-8 ">
-                <Link to={`/projects/project/${id}`}>
-                    <img 
-                        src={img} 
-                        alt={id}
-                        className="max-w-full h-auto"
-                    />
-                </Link>
-                <h3 className="text-white text-2xl mt-6 py-2 tracking-widest font-semibold">{name}</h3>
-                <h3 className="text-gray-500 text-lg font-light leading-9 tracking-tight">{title}</h3>
-                <button className="mt-6 bg-slate-700 rounded-md px-6 font-light py-4 hover:text-slate-40    0 hover:bg-slate-800">
-                    <Link to={`${address}`}>
-                        VIEW PROJECT
-                    </Link> 
-                </button>
-                     
+        <> 
+            <div className="flex justify-center md:p-4 px-0">
+                <SmoothScrolling>
+                    <div className="project-bg rounded-sm mb-3 p-8 ">
+                        <Link to={`/projects/project/${id}`}>
+                            <img 
+                                src={img} 
+                                alt={id}
+                                className="max-w-full h-auto"
+                            />
+                        </Link>
+                        <h3 className="text-white text-2xl mt-6 py-2 tracking-widest font-semibold">{name}</h3>
+                        <h3 className="text-gray-500 text-lg font-light leading-9 tracking-tight">{title}</h3>
+                        <button className="mt-6 bg-slate-700 rounded-md px-6 font-light py-4 hover:text-slate-40    0 hover:bg-slate-800">
+                            <Link to={`${address}`}>
+                                VIEW PROJECT
+                            </Link> 
+                        </button>
+                            
+                    </div>
+                </SmoothScrolling>
             </div>
-            </SmoothScrolling>
-        </div>
+        </>
     )
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -4,6 +4,8 @@ import { Banner } from "../component/Banner"
 import { PortfolioContext } from "../context/portfolioContext"
 import { ContactPartial } from "../component/ContactPartial"
 import { ContactForm } from "../component/ContactForm"
+import Meta from "../component/Meta"
+import { BreadCrumb } from "../component/BreadCrumb"
 
 export const Project = (props) => {
     const ctx = useContext(PortfolioContext)
@@ -16,7 +18,25 @@ export const Project = (props) => {
     const { title, name, address, img, summary, techStack } = project
     return(
         <div className="py-8">
-            <Banner/>
+            <div className="banner-wrapper">
+                <div className="banner">
+                    <div className="py-2">
+                        <h2 className="text-5xl font-semibold text-center py-2">Single Project</h2>
+                        <p className="text-gray-300 text-center font-light text-3xl py-2 leading-8 tracking-wider">{name}</p>
+                    </div>
+                    <Meta title={"Project"}/>
+                    <BreadCrumb title="Project"/>
+                    <div className="flex justify-center py-2">
+                        <div>
+                            <svg width="50px" height="100px" viewBox="0 0 247 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{fillRule:"evenodd" ,clipRule:"evenodd", strokeLinecap:"round" ,strokeLinejoin:'round', strokeMiterlimit:'1.5'}}>
+                                <path  className="wheel" d="M123.359,79.775l0,72.843" style={{fill:"none" ,stroke:"#fff" ,strokeWidth:"15px"}} />
+                                <path  className="scroll" d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z" style={{fill:"none" ,stroke:"#fff" ,strokeWidth:"15px"}} />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <section className="mt-6 py-6 md:grid grid-cols-1 gap-4 mb-4 mx-4 md:mx-32 lg:mx-60 xl:mx-80">
                 <div className="bg-dark  mb-4">
                     <div className="my-12">
