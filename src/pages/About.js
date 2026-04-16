0cc60dc64c1867962426f3875b39324483bc610b
= <mmohmo@yahoo.com>
2026-04-16 12:55:44 -0400

    profile picture added

--- src/pages/About.js
+++ src/pages/About.js
@@ -61,7 +61,7 @@ export const About = () => {
             <div></div>
             <div className="col-span-2">
               <SmoothScrolling>
-                <img className="rounded-lg w-30 h-30 object-cover" src="/images/port.jpg" alt="profile"/>
+                <img className="rounded-lg w-30 h-30 object-cover" src="/images/profile_pic_26.PNG" alt="profile"/>
               </SmoothScrolling>
             </div>
             <div></div>
@@ -174,7 +174,7 @@ export const About = () => {
                 <div>
                   <h4 className="text-lg text-gray-300 font-thin tracking-tight">
                     <SmoothScrolling>
-                      MetroSport
+                      Metro Sports
                     </SmoothScrolling>
                   </h4>
                   <h3 className="text-xs text-gray-100 font-semibold tracking-wider">
@@ -199,6 +199,40 @@ export const About = () => {
                 EDUCATION
               </SmoothScrolling>
             </h3>
+
+            <div 
+              className="flex justify-between items-center border-b border-slate-800 py-4"
+            >
+              <div className="flex items-center justify-betwen">
+                <SmoothScrolling>
+                  <div className="flex items-center pl-2 h-12 w-12 bg-zinc-700 rounded-full mr-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                    </svg>
+                  </div>
+                </SmoothScrolling>
+                <div>
+                  <h4 className="text-lg text-gray-300 font-thin tracking-tight">
+                    <SmoothScrolling>
+                      Manhattan School of Computer Technology
+                    </SmoothScrolling>
+                  </h4>
+                  <h4 className="text-xs text-gray-100 font-semibold tracking-wider">
+                    <SmoothScrolling>
+                      Medical Office Specialist Program
+                    </SmoothScrolling>
+                  </h4>
+                </div>
+              </div>
+              <div>
+                <h3 className="text-lg text-gray-300 font-thin tracking-tight">
+                  <SmoothScrolling>
+                    2026
+                  </SmoothScrolling>
+                </h3>
+              </div>
+            </div>
+
             <div 
               className="flex justify-between items-center border-b border-slate-800 py-4"
             >
